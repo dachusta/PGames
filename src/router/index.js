ba370eb6dedcd9c8f36de0e1e09a9e1c349a17f3
ChustaD <chusta2013@mail.ru>
2022-04-23 18:24:05 +0300

    Add some html and css

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/VList.vue')
+    component: () => import(/* webpackChunkName: "about" */ '@/views/VList.vue')
   },
   {
     path: '/v-item',
@@ -21,7 +21,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/VItem.vue')
+    component: () => import(/* webpackChunkName: "about" */ '@/views/VItem.vue')
   }
 ]
 
--- src/views/VItem.vue
+++ src/views/VItem.vue
@@ -1,16 +1,29 @@
 <template>
-  <div>
-    <div class="name">{{ gameItem.name }}</div>
-    <div class="name">{{ gameItem.description }}</div>
-    <div class="name" :style="{ background: '#'+gameItem.dominant_color }">{{ gameItem.dominant_color }}</div>
-    <div class="name">{{ gameItem.playtime }}</div>
-    <div class="name">{{ gameItem.released }}</div>
-    <div class="name">{{ gameItem.metacritic }}</div>
-    <div class="name">{{ gameItem.esrb_rating }}</div>
-    <div class="name">{{ gameItem.background_image }}</div>
-    <div class="name">{{ gameItem.added }}</div>
-    <div class="name">{{ gameItem.added_by_status }}</div>
-    <div class="name">{{ gameItem.tags }}</div>
+  <div class="page game-item">
+    <div class="wrap right screenshots-list?">
+      <img
+        class="screenshots-item"
+        v-for="screenshot in gameScreenshots.results"
+        :key="screenshot.id"
+        :src="screenshot.image"
+        alt="1"
+      />
+    </div>
+    <div class="left">
+      <div class="name">{{ gameItem.name }}</div>
+      <div class="description">{{ gameItem.description_raw }}</div>
+    </div>
+    <div class="details">
+      <div class="name" :style="{ background: '#'+gameItem.dominant_color }">{{ gameItem.dominant_color }}</div>
+      <div class="name">{{ gameItem.playtime }}</div>
+      <div class="name">{{ gameItem.released }}</div>
+      <div class="name">{{ gameItem.metacritic }}</div>
+      <div class="name">{{ gameItem.esrb_rating }}</div>
+      <div class="name">{{ gameItem.background_image }}</div>
+      <div class="name">{{ gameItem.added }}</div>
+      <div class="name">{{ gameItem.added_by_status }}</div>
+      <div class="name">{{ gameItem.tags }}</div>
+    </div>
   </div>
 </template>
 
@@ -23,21 +36,65 @@ import { useRoute } from 'vue-router'
 // })
 
 const gameItem = ref({})
+const gameScreenshots = ref([])
 const route = useRoute()
 
 console.log(route.params.id)
 
-async function api (id) {
+async function api (id = 3498) {
   const url = `https://api.rawg.io/api/games/${id}?key=8f4899e2e65a42e58807bc9bbec35cca`
   const response = await fetch(url)
   const commits = await response.json()
   console.log(commits)
   gameItem.value = commits
 }
-onMounted(api(route.params.id))
+async function apiScreenshots (id = 3498) {
+  const url = `https://api.rawg.io/api/games/${id}/screenshots?key=8f4899e2e65a42e58807bc9bbec35cca`
+  const response = await fetch(url)
+  const commits = await response.json()
+  console.log(commits)
+  gameScreenshots.value = commits
+
+  console.log(gameScreenshots.value.results)
+}
+onMounted(api(route.params.id), apiScreenshots(route.params.id))
 
 </script>
 
 <style lang="scss" scoped>
-
+.game-item {
+  display: grid;
+  grid-template-areas:
+    "screenshots description"
+    "details details";
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  gap: 30px;
+  background: #1C1C2C;
+  color: #CBDBEE;
+}
+.right {
+  display: flex;
+  flex-wrap: wrap;
+  grid-area: screenshots;
+}
+.screenshots-item {
+  width: 250px;
+  height: 130px;
+}
+.left {
+  grid-area: description;
+  padding: 10px 20px;
+  background: #3C4464;
+}
+.name {
+  font-size: 18px;
+}
+.description {
+  font-size: 14px;
+  white-space: pre-wrap;
+}
+.details {
+  grid-area: details;
+}
 </style>
