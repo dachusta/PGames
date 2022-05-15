691861dae5d64cb15b5a8bbab4b2fa964962d013
ChustaD <chusta2013@mail.ru>
2022-05-15 15:31:21 +0300

    Fix route

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
     path: '/v-list',
     name: 'VList',
+    alias: '/',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
