236efdbf7860e7489230e8797fc79cafc794967f
ChustaD <chusta2013@mail.ru>
2022-06-11 15:59:16 +0300

    Add fonts

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,11 @@
 <style lang="scss">
 @import url("@/assets/css/normalize");
 
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Open Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
